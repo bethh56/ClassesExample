32f690279bffb1e4f4a3fedd74a8c2a8685b380d
Beth <bethhnielsen56@gmail.com>
2020-08-03 20:51:29 -0500

    Add project files.

---
+++ ClassesExample/Animals/Chicken.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExample.Animals
+{
+    class Chicken
+    {
+        // property 
+        public string Name { get; set; }
+
+        // field
+        // default value of bool false 
+        private bool _isHungry = true;
+
+        //constructor
+        public Chicken(string name)
+        {
+            Name = name;
+            Console.WriteLine($"{name} got instantiated...");
+        }
+        public void Peck(string typeOfFood)
+        {
+            if (_isHungry)
+            {
+                Console.WriteLine($"{Name} pecks hungrily at the {typeOfFood}");
+                _isHungry = false;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is not hungery");
+            }
+        }
+    }
+}
---
+++ ClassesExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using ClassesExample.Animals;
+using System;
+
+// namespace = large, high level bundle of code
+// using System is an example of a namespace
+namespace ClassesExample
+{
+    class Program
+    {
+        //method is a function inside a class
+        // all methods are functions but not all functions are methods
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var chicken = new Chicken("Liza");
+            var bob = new Chicken("Bob");
+
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+            chicken.Peck("corn");
+
+            bob.Peck("watermelon");
+        }
+    }
+
+}
+
+// method signature
+// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/methods
+// first part is access method
+// what kind of thing you will return
+// name of method 
+// parenthesis and arguement parameter
+// public void Peck(string typeOfFood)
